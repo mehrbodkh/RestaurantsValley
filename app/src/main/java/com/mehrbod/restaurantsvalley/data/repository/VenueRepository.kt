c1314e7c09bc2704d6cf4c46d50bc3b2087ac02f
Mehrbod <mehrbod.kh@gmail.com>
2021-08-12 14:40:44 +0430

    Implement venues' search in repository

--- app/src/main/java/com/mehrbod/restaurantsvalley/data/repository/VenueRepository.kt
+++ app/src/main/java/com/mehrbod/restaurantsvalley/data/repository/VenueRepository.kt
@@ -1,4 +1,8 @@
 package com.mehrbod.restaurantsvalley.data.repository
 
+import com.mehrbod.restaurantsvalley.data.model.response.Venues
+import kotlinx.coroutines.flow.Flow
+
 interface VenueRepository {
+    fun getVenues(lat: Double, lng: Double, radius: Int): Flow<Result<List<Venues>>>
 }
--- app/src/main/java/com/mehrbod/restaurantsvalley/data/repository/VenueRepositoryImpl.kt
+++ app/src/main/java/com/mehrbod/restaurantsvalley/data/repository/VenueRepositoryImpl.kt
@@ -1,11 +1,28 @@
 package com.mehrbod.restaurantsvalley.data.repository
 
 import com.mehrbod.restaurantsvalley.data.datasource.VenuesDataSource
+import com.mehrbod.restaurantsvalley.data.model.response.Venues
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 import javax.inject.Named
 
 class VenueRepositoryImpl @Inject constructor(
-    @Named("RemoteSource") remoteDataSource: VenuesDataSource
+    @Named("RemoteSource") private val remoteDataSource: VenuesDataSource,
+    @Named("IODispatcher") private val dispatcher: CoroutineDispatcher
 ) : VenueRepository {
 
+    override fun getVenues(lat: Double, lng: Double, radius: Int): Flow<Result<List<Venues>>> =
+        flow {
+            try {
+                val result = remoteDataSource.fetchVenues(lat, lng, radius)
+                emit(Result.success(result.response.venues))
+            } catch (e: Exception) {
+                e.cause?.let {
+                    emit(Result.failure<List<Venues>>(Throwable("Request did not have a response.")))
+                }
+            }
+        }.flowOn(dispatcher)
 }
