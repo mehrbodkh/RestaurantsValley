f5168942bf2becbebb565e4a45697d4dd4100e58
Mehrbod <mehrbod.kh@gmail.com>
2021-08-12 15:26:24 +0430

    Add test for repository

--- app/src/main/java/com/mehrbod/restaurantsvalley/data/repository/VenueRepositoryImpl.kt
+++ app/src/main/java/com/mehrbod/restaurantsvalley/data/repository/VenueRepositoryImpl.kt
@@ -4,6 +4,7 @@ import com.mehrbod.restaurantsvalley.data.datasource.VenuesDataSource
 import com.mehrbod.restaurantsvalley.data.model.response.Venues
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
@@ -16,13 +17,13 @@ class VenueRepositoryImpl @Inject constructor(
 
     override fun getVenues(lat: Double, lng: Double, radius: Int): Flow<Result<List<Venues>>> =
         flow {
-            try {
-                val result = remoteDataSource.fetchVenues(lat, lng, radius)
+            val result = remoteDataSource.fetchVenues(lat, lng, radius)
+            if (result.meta.code == 200) {
                 emit(Result.success(result.response.venues))
-            } catch (e: Exception) {
-                e.cause?.let {
-                    emit(Result.failure<List<Venues>>(Throwable("Request did not have a response.")))
-                }
+            } else {
+                emit(Result.failure<List<Venues>>(Throwable("")))
             }
-        }.flowOn(dispatcher)
+        }
+            .flowOn(dispatcher)
+            .catch { emit(Result.failure(it)) }
 }
