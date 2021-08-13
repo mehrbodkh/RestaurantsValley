536374dbd284c761f34cf267d670b98585f9f7fe
Mehrbod <mehrbod.kh@gmail.com>
2021-08-13 11:28:24 +0430

    Add venue repository to venue on map view model

--- app/src/main/java/com/mehrbod/restaurantsvalley/presentation/venueonmap/VenueOnMapViewModel.kt
+++ app/src/main/java/com/mehrbod/restaurantsvalley/presentation/venueonmap/VenueOnMapViewModel.kt
@@ -1,7 +1,13 @@
 package com.mehrbod.restaurantsvalley.presentation.venueonmap
 
 import androidx.lifecycle.ViewModel
+import com.mehrbod.restaurantsvalley.data.repository.VenueRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class VenueOnMapViewModel : ViewModel() {
+@HiltViewModel
+class VenueOnMapViewModel @Inject constructor(
+    private val venueRepository: VenueRepository
+) : ViewModel() {
 
 }
